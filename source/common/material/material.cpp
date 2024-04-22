b3c21be84d613e2affda3dabb4c62c162551b807
Omar Said <osazizsg1@gmail.com>
2024-04-22 18:07:10 +0200

    add lit material

--- source/common/material/material.cpp
+++ source/common/material/material.cpp
@@ -70,5 +70,64 @@ namespace our {
         texture = AssetLoader<Texture2D>::get(data.value("texture", ""));
         sampler = AssetLoader<Sampler>::get(data.value("sampler", ""));
     }
+    void litMaterial::setup() const
+    {
+        Material::setup();
+        if (albedo){
+            glActiveTexture(GL_TEXTURE0);
+            albedo->bind();
+            if(samplerAlbedo){
+                samplerAlbedo->bind(0);
+            }
+            shader->set("material.albedo", 1);
+        }
+        if (specular){
+            glActiveTexture(GL_TEXTURE1);
+            specular->bind();
+            if(samplerSpecular){
+                samplerSpecular->bind(1);
+            }
+            shader->set("material.specular", 1);
+        }
+        if (roughness){
+            glActiveTexture(GL_TEXTURE2);
+            roughness->bind();
+            if(samplerRoughness){
+                samplerRoughness->bind(2);
+            }
+            shader->set("material.roughness", 2);
+        }
+        if (ambientOcclusion){
+            glActiveTexture(GL_TEXTURE3);
+            ambientOcclusion->bind();
+            if(samplerAmbientOcclusion){
+                samplerAmbientOcclusion->bind(3);
+            }
+            shader->set("material.ambientOcclusion", 3);
+        }
+        if (emissive){
+            glActiveTexture(GL_TEXTURE4);
+            emissive->bind();
+            if(samplerEmissive){
+                samplerEmissive->bind(4);
+            }
+            shader->set("material.emissive", 4);
+        }
+    }
+    void litMaterial::deserialize(const nlohmann::json& data)
+    {
+        Material::deserialize(data);
+        if(!data.is_object()) return;
+        albedo = AssetLoader<Texture2D>::get(data.value("albedo", ""));
+        specular = AssetLoader<Texture2D>::get(data.value("specular", ""));
+        roughness = AssetLoader<Texture2D>::get(data.value("roughness", ""));
+        ambientOcclusion = AssetLoader<Texture2D>::get(data.value("ambientOcclusion", ""));
+        emissive = AssetLoader<Texture2D>::get(data.value("emissive", ""));
+        samplerAlbedo = AssetLoader<Sampler>::get(data.value("samplerAlbedo", ""));
+        samplerSpecular = AssetLoader<Sampler>::get(data.value("samplerSpecular", ""));
+        samplerRoughness = AssetLoader<Sampler>::get(data.value("samplerRoughness", ""));
+        samplerAmbientOcclusion = AssetLoader<Sampler>::get(data.value("samplerAmbientOcclusion", ""));
+        samplerEmissive = AssetLoader<Sampler>::get(data.value("samplerEmissive", ""));
+    }
 
 }
--- source/common/material/material.hpp
+++ source/common/material/material.hpp
@@ -52,7 +52,23 @@ namespace our {
         void setup() const override;
         void deserialize(const nlohmann::json& data) override;
     };
+    class litMaterial : public Material {
+        public:
+        Texture2D* albedo;
+        Texture2D* specular;
+        Texture2D* roughness;
+        Texture2D* ambientOcclusion;
+        Texture2D* emissive;
+        Sampler* samplerAlbedo;
+        Sampler* samplerSpecular;
+        Sampler* samplerRoughness;
+        Sampler* samplerAmbientOcclusion;
+        Sampler* samplerEmissive;
+        void setup() const override;
+        void deserialize(const nlohmann::json& data) override;
 
+
+    };
     // This function returns a new material instance based on the given type
     inline Material* createMaterialFromType(const std::string& type){
         if(type == "tinted"){
